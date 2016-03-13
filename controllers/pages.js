f60a2b5f3e7e898105417ff30f3f697594579581
Anna1004 <anna.khlutkova@mail.ru>
2016-03-13 15:09:08 +0500

    feat(controllers): добавлен рендер на регистрацию

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,6 +1,10 @@
 exports.index = (req, res) => {
     res.render('authorization/authorization', {});
 };
+exports.reg = (req, res) => {
+    res.render('registration/registration', {});
+};
+
 
 exports.error404 = (req, res) => {
     res.sendStatus(404);
