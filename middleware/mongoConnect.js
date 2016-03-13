c6a9bcab3d71756e0926bd3553c7380c3bc83a1d
Savi <Ultimate95@mail.ru>
2016-03-13 17:37:43 +0500

    fix(middleware/mongoConnect.js): hot fix

--- middleware/mongoConnect.js
+++ middleware/mongoConnect.js
@@ -9,10 +9,11 @@ module.exports = (mongoUri) => {
         if (!db) {
             db = MongoClient.connect(mongoUri, (err, db) => {
                 if (err) {
-                    next(new Error('failed to connect mongo'));
+                    next(err);
+                } else {
+                    req.db = db;
+                    next();
                 }
-                req.db = db;
-                next();
             });
         } else {
             req.db = db;
