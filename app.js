39ded920c090a5575fe17668eb771afd7b75b0a0
Dolgih Natalia <n.e.dolgih@yandex.ru>
2016-03-13 12:25:01 +0500

    Добавление handlebars

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
