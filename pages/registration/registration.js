90b70a936c632e3e5e754969a6881da93c333aff
Anna1004 <anna.khlutkova@mail.ru>
2016-03-13 17:33:13 +0500

    fix(blocks): обновление

--- pages/registration/registration.js
+++ pages/registration/registration.js
@@ -5,5 +5,12 @@ require('../../blocks/grid/grid.css');
 require('../../blocks/footer/footer.css');
 require('./registration.css');
 
+$('#submit').click(function (e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    var password = $('#password').val();
+    var email = $('#email').val();
+    console.log(name);
+})
 
 
