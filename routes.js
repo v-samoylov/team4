c6165bd951441dc6af34d12c07f1a6eac27e17f1
Dolgih Natalia <n.e.dolgih@yandex.ru>
2016-03-13 12:05:40 +0500

    Создание express приложения

---
+++ routes.js
@@ -0,0 +1,12 @@
+
+const pages = require('./controllers/pages');
+
+module.exports = function (app) {
+    app.get('/', pages.index);
+    app.all('*', pages.error404);
+
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.sendStatus(500);
+    })
+};
