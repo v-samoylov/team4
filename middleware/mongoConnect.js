e901b4f1ff44afdad60d2094822bd5c0a667edc9
PolinaKoval <polina.koval.32@gmail.com>
2016-03-13 15:35:49 +0500

    mongo2

--- middleware/mongoConnect.js
+++ middleware/mongoConnect.js
@@ -7,7 +7,7 @@ module.exports = (mongoUri) => {
     mongoUri = mongoUri || 'mongodb://team4:DreamTeam@ds011449.mlab.com:11449/team4hackaton';
     return (req, res, next) => {
         if (!db) {
-            db = MongoClient.connect(testUri, (err, db) => {
+            db = MongoClient.connect(mongoUri, (err, db) => {
                 if (err) {
                     next(new Error('failed to connect mongo'));
                 }
