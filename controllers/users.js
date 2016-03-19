1ef599676df162505dac387223006364b5beb50f
v-samoylov <samoylov.vladimir@yahoo.com>
2016-03-19 17:54:13 +0500

    fix(controllers/users.js): Убран пробел в регулярке для имен

--- controllers/users.js
+++ controllers/users.js
@@ -68,7 +68,7 @@ module.exports.validate = (req, res, next) => {
         }
         req.body.name = req.body.name.trim();
         if (
-            !req.body.name.match(/^[А-яA-z \-0-9]{2,30}$/) ||
+            !req.body.name.match(/^[А-яA-z\-0-9]{2,30}$/) ||
             !req.body.name.match(/[А-яA-z]+/)
         ) {
             res.status(400).send({message: 'Name is not valid', status: 'Error'});
