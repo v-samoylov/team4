5c255b7050649cb8f4b201c90c7093e26bf70fb4
v-samoylov <samoylov.vladimir@yahoo.com>
2016-03-19 17:50:23 +0500

    fix(controllers/users.js): замена \s на пробел + ограничения на кол-во знаков

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,10 @@ module.exports.validate = (req, res, next) => {
         res.status(400).send({message: 'Email is not valid', status: 'Error'});
         return;
     }
-
+    if (req.body.password.length > 30) {
+        res.status(400).send({message: 'Password is not valid', status: 'Error'});
+        return;
+    }
     if (req.path === '/user/reg') {
         if (!req.body.name) {
             res.status(400).send({message: 'Name is required', status: 'Error'});
@@ -65,7 +68,7 @@ module.exports.validate = (req, res, next) => {
         }
         req.body.name = req.body.name.trim();
         if (
-            !req.body.name.match(/^[А-яA-z\s\-0-9]+$/) ||
+            !req.body.name.match(/^[А-яA-z \-0-9]{2,30}$/) ||
             !req.body.name.match(/[А-яA-z]+/)
         ) {
             res.status(400).send({message: 'Name is not valid', status: 'Error'});
