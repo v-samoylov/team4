b6b6268e3b52d9d2a3b441d6c5226b05edbc3e96
v-samoylov <v-samoylov@users.noreply.github.com>
2016-04-06 12:00:57 +0500

    req.user => req.commonData.user
    * update(controllers/users, routes.js, middleware/cookieAuthenticator)
    
    * update(lib/hash)
    
    * req.user => req.commonData.user
    
    * req.user => req.commonData.user

--- middleware/cookieAuthenticator.js
+++ middleware/cookieAuthenticator.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const hash = require('../lib/hash.js');
-const config = require('config');
-const hashConfig = config.get("hash");
+const salt = require('config').get("hash").cookieSalt;
 const debug = require('debug')('team4:middleware:cookieAuthenticator');
-const salt = hashConfig.cookieSalt;
 
 module.exports = () => {
     return (req, res, next) => {
         debug('check cookie');
-        req.user = {};
+        if (req.commonData) {
+            req.commonData.user = {};
+        } else {
+            req.commonData = {user: {}};
+        }
         var userId = req.cookies.id;
         if (userId) {
             var isLoggedIn = hash.validate(userId, salt);
             var userName = userId.split('.')[0];
             if (isLoggedIn) {
-                req.user.name = userName;
+                req.commonData.user.name = userName;
             }
         }
         next();
