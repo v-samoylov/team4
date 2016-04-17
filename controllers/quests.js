680fec6ec4f655197b35b5f53a6526552813e3fd
Наташа <n.e.dolgih@yandex.ru>
2016-04-17 16:35:21 +0500

    feat(.\controllers\quest.js): Добавлен контроллер добавления квестов

--- controllers/quests.js
+++ controllers/quests.js
@@ -1,5 +1,16 @@
-const debug = require('debug')('team4:controllers:quests');
+'use strict';
 
-module.exports.addQuest = () => {
+const debug = require('debug')('team4:controllers:quests');
+const questsModel = require('../models/quests.js');
+
+module.exports.addQuest = (req, res) => {
     debug();
+    questsModel.createQuest(req.body.quest).then(
+        () => {
+            res.status(200).send('Place added successfully');
+        },
+        error => {
+            res.status(400).send(error.message);
+        }
+    )
 };
