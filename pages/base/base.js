d4d072b6e8fda84c7d9beb8b31ad4738686d12ba
NekrasovAlexey <creepy404nf@gmail.com>
2016-05-15 15:57:01 +0500

    fix() remove comments

--- pages/base/base.js
+++ pages/base/base.js
@@ -3,8 +3,6 @@
 require('./base.css');
 require('../../blocks/header/header.js');
 require('../../blocks/footer/footer.js');
-// require('../../blocks/header/header.css');
-// require('../../blocks/footer/footer.css');
 
 $('#logout').click(function (e) {
     e.preventDefault();
