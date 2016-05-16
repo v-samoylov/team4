3d6fc33d6917d4466f6b91d30585f16054102937
PolinaKoval <polina.koval.32@gmail.com>
2016-05-17 02:14:09 +0600

    страница квеста (#152)
    * методы для квестов в models/users
    
    * обновления
    
    * обновления
    
    * отступы
    
    * new version
    
    * local change2
    
    * quest view
    
    * fix
    
    * merged
    
    * userPage
    
    * userPage
    
    * checkin
    
    * all
    
    * comments
    
    * fix
    
    * fix2

--- blocks/place/place.js
+++ blocks/place/place.js
@@ -1,35 +1,47 @@
+require('./place.css');
+
+function checkIn() {
+    var name = $(this).data('name');
+    var options = {
+        enableHighAccuracy: true,
+        maximumAge: 50000,
+        timeout: 10000
+    };
+    var button = this;
+
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            $.ajax({
+                url: '/quest/checkin/',
+                type: 'POST',
+                data: {
+                    name: name,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                }
+            })
+                .done(function () {
+                    console.log('checkIn');
+                    var checkIn = $('<span></span>', {
+                        class: 'glyphicon glyphicon-ok-circle success-checkIn'
+                    });
+                    $(button).replaceWith(checkIn);
+                })
+                .fail(function (err) {
+                    console.log(err);
+                });
+        },
+        function (error) {
+            console.log(error);
+        },
+        options
+    );
+}
+
+module.exports.checkIn = checkIn;
+
 $(function () {
     $('.check-in').each(function () {
-        $(this).click(function () {
-            var name = $(this).data('name');
-            var options = {
-                enableHighAccuracy: true,
-                maximumAge: 50000,
-                timeout: 10000
-            };
-            navigator.geolocation.getCurrentPosition(
-                function (position) {
-                    $.ajax({
-                        url: '/quest/checkin/',
-                        type: 'POST',
-                        data: {
-                            name: name,
-                            latitude: position.coords.latitude,
-                            longitude: position.coords.longitude
-                        }
-                    })
-                        .done(function () {
-                            console.log('Check-in');
-                        })
-                        .fail(function (err) {
-                            console.log(err);
-                        });
-                },
-                function (error) {
-                    console.log(error);
-                },
-                options
-            );
-        });
+        $(this).click(checkIn);
     });
 });
--- pages/quest/quest.js
+++ pages/quest/quest.js
@@ -1,22 +1,10 @@
 'use strict';
 
 require('./quest.css');
-require('../../blocks/place/place.js');
 require('../../blocks/comments/comments.js');
+var checkInFunc = require('../../blocks/place/place.js').checkIn;
 
 $(function () {
-    $('.panel.main > .panel-heading').each(function () {
-        $(this).click(function () {
-            var last = $(this).next();
-
-            if ($(last).css('display') === 'none') {
-                $(last).css('display', 'block');
-            } else {
-                $(last).css('display', 'none');
-            }
-        });
-    });
-
     $('#quest-like').change(function () {
         var title = $('#quest-title').html();
         var valueInput = $('#likes-count');
@@ -45,6 +33,54 @@ $(function () {
         });
     });
 
+    $('#review').submit(function (event) {
+        event.preventDefault();
+        var form = $(this);
+        var text = form.find('.new-comment').val();
+        var name = form.data('name');
+        var commentSection = form.prev('section');
+
+        $.ajax({
+            method: 'POST',
+            url: '/quest-comment',
+            data: {
+                name: name, text: text
+            }
+        })
+            .done(function (msg) {
+                var comment = $('<div></div>', {
+                    class: 'review'
+                });
+                var heading = $('<div></div>', {
+                    class: 'review-heading'
+                });
+                var body = $('<div></div>', {
+                    class: 'review-body',
+                    text: msg.text
+                });
+                heading.appendTo(comment);
+                $('<hr>').appendTo(comment);
+                body.appendTo(comment);
+                $('<span></span>', {
+                    class: 'glyphicon glyphicon-user',
+                    'aria-hidden': true
+                }).appendTo(heading);
+                $('<a></a>', {
+                    href: '/user/' + msg.url,
+                    class: 'user-link',
+                    text: ' ' + msg.author
+                }).appendTo(heading);
+
+                commentSection.append(comment);
+
+                form.find('textarea').val('');
+                form.find('[type="submit"]').prop('disabled', true);
+            })
+            .fail(function (msg) {
+                console.log(msg);
+            });
+    });
+
     /*  eslint quote-props: [1, "as-needed"] */
     $('#start-quest').click(function () {
         var title = $('#quest-title').html();
@@ -67,7 +103,7 @@ $(function () {
                     text: 'Check-in',
                     'data-name': name
                 });
-
+                checkIn.click(checkInFunc);
                 $(this).append(checkIn);
             });
         })
