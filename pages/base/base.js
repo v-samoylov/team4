4dfe2e8c3c1a8780e91e421d646516659bec9f75
Savichev Igor <Ultimate95@mail.ru>
2016-05-18 18:15:58 +0500

    Update base.js

--- pages/base/base.js
+++ pages/base/base.js
@@ -6,4 +6,20 @@ require('../../blocks/footer/footer.js');
 
 $(function () {
     autosize($('textarea')); // eslint-disable-line
+
+     $('#logout').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            method: 'POST',
+            url: '/user/logout'
+        })
+        .done(function (msg) {
+            console.log(msg);
+            window.location = '/';
+        })
+        .fail(function (msg) {
+            console.log(msg);
+        });
+    });
 });
